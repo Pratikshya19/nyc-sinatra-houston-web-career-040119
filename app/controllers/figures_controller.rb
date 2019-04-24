0f3320ab47fd83f1997df85106b057ca58e10a3c
github username <github email address>
2019-04-24 18:29:59 -0500

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,62 @@
 class FiguresController < ApplicationController
   # add controller methods
+
+get '/figures' do
+  @figures = Figure.all
+erb :'figures/index'
 end
+
+get '/figures/new' do
+  @titles = Title.all
+  erb :'figures/new'
+
+
+end
+
+get '/figures/:id' do
+  @figure = current_figure
+
+  erb :'figures/show'
+end
+
+
+#
+post '/figures' do
+  # @figure = Figure.create([params['figure'])
+
+  redirect '/figures'
+end
+
+
+get '/figures/:id/edit' do
+  @figure = current_figure
+  erb :'figures/edit'
+end
+
+#edit an existing figure
+
+patch '/figures/:id' do
+  figure = current_figure
+  figure.update(name: params[:name])
+  redirect "/figures/#{params[:id]}"
+end
+
+
+#delete an existing figure
+
+delete '/figures/:id' do
+figure = current_figure
+  figure.destroy
+  redirect "/figures"
+end
+
+
+
+
+
+def current_figure
+  Figure.find(params[:id]) 
+end
+
+end
+
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,59 @@
 class LandmarksController < ApplicationController
   # add controller methods
+
+
+
+  get '/landmarks' do
+    @landmarks = Landmark.all
+  erb :'landmarks/index'
+  end
+  
+  get '/landmarks/new' do
+    erb :'landmarks/new'
+  
+  end
+  
+  get '/landmarks/:id' do
+    @landmark = current_landmark
+  
+    erb :'landmarks/show'
+  end
+  
+  post 'landmarks' do
+    # Landmark.create(params)
+    redirect '/landmarks'
+  end
+  
+  
+  get '/landmarks/:id/edit' do
+    @landmark = current_landmark
+    erb :'landmarks/edit
+  end
+  
+  
+  
+  patch '/landmarks/:id' do
+    
+    landmark = current_landmark
+    landmark.update(name: params[:name])
+    redirect "/landmarks/#{params[:id]}"
+  end
+  
+  
+ 
+  
+  delete '/landmarks/:id' do
+  landmark = current_landmark
+    landmark.destroy
+    redirect "/landmarks"
+  end
+  
+  
+  
+  
+  
+  def current_landmark
+    Landmark.find(params[:id]) 
+  end
+  
 end
